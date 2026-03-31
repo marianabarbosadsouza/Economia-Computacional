4e27614ab8166e9185c815143e9d5c40e5a8d77d
marianabarbosadsouza <marianabarbosadsouza@gmail.com>
2026-03-31 12:50:03 -0300

    Add files via upload

---
+++ Aula 240326/Adulto/Adulto_001.py
@@ -0,0 +1,26 @@
+"""
+Programa Adulto
+Descrição: Este programa pergunta a idade de uma pessoa. Se a
+idade for maior do que 18 anos, o programa imprime na tela o
+texto ”Oi! Você é um adulto.
+Data: 24/03/2026
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+idade = 0
+texto = ""
+
+# Entrada de dados
+
+idade = int(input("Qual a sua idade?"))
+
+# Processamento de dados
+
+if idade >= 18:
+    texto = 'Oi! Você é um adulto.'
+
+# Saída de dados
+
+print(texto)
